f815f23bb55bd9c1b5d1e4d5d723345a8e5fdb69
Thomas Parry <yrrapt@gmail.com>
2021-10-02 21:48:07 +0200

    Changed name of module

--- analog_sim/plot/plot_bode.py
+++ analog_sim/plot/plot_bode.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
 import os, sys
-from yaaade.spice.generic import GenericSpiceInterface
-from yaaade.plot.plot import * 
+from analog_sim.spice.generic import GenericSpiceInterface
+from analog_sim.plot.plot import * 
 
 print(sys.argv[1])
 
 
 # create the object
-yaaade_obj = GenericSpiceInterface()
+analog_sim_obj = GenericSpiceInterface()
 
 # read in the results
-yaaade_obj.read_results(sys.argv[1])
+analog_sim_obj.read_results(sys.argv[1])
 
 # plot the results
-plot_bode(yaaade_obj, 'v(ac)', linewidth=1, alpha=1)
+plot_bode(analog_sim_obj, 'v(ac)', linewidth=1, alpha=1)
